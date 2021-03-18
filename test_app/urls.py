8fdd5770d45bcb4dffe0755be2dd1b97743f20d7
ringomag <optimuskrajm@gmail.com>
2021-03-18 17:51:44 +0100

    Bubi help!

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 from .views import IndexView
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', IndexView.as_view(), name="index"),
-    
+    path('', IndexView.as_view(), name="index"),   #u indexu hocu samo formu za unos i postovanje
+    # path('added_item.html', IndexView.as_view(), name="added"),  ovo sam probao al onda dobijam na toj strani isto sto i na indexu
+    path('added_item.html', TemplateView.as_view(template_name="added_item.html"), name="added" ), #ovde samo get metodu - samo prikaz liste
 ]
--- test_app/views.py
+++ test_app/views.py
@@ -7,19 +7,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 class IndexView(View):
-    form = LikForm
-    initial = {'key': 'value'}
+    form = LikForm  #ovo je forma koju uzimam po modelu
+    initial = {'key': 'value'}   #inicijalne
     template_name="index.html"
     def get(self, request, *args, **kwargs):
 
-        created = Lik.objects.all()
-        form = self.form(initial=self.initial)
+        created = Lik.objects.all()     #ovde vucem podatke iz baze i prikazujem ih posle kroz created:created
+        form = self.form(initial=self.initial)    #ovo je forma koju koristim i prikazujem kroz form:form
         return render(request, self.template_name, {"created":created,"form":form})
 
     def post(self, request, *args, **kwargs):
-        form = LikForm
+        template_name = 'added_item.html'
         form = self.form(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
-        return render(request, self.template_name, {"form":form})
+            return redirect('added')  #ovde mozda gresim
+        return render(request, self.template_name, {"form":form})    #ovde mozda gresim
