f73130f2201de56556b0b510621d5c8751c6eaf6
ringomag <optimuskrajm@gmail.com>
2021-03-19 17:39:44 +0100

    it works

--- test_app/urls.py
+++ test_app/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', IndexView.as_view(), name="index"),   #u indexu hocu samo formu za unos i postovanje
-    # path('added_item.html', IndexView.as_view(), name="added"),  ovo sam probao al onda dobijam na toj strani isto sto i na indexu
-    path('added_item.html', TemplateView.as_view(template_name="added_item.html"), name="added" ), #ovde samo get metodu - samo prikaz liste
+    path('', IndexView.as_view(template_name='index.html'), name="index"),   #u indexu hocu samo formu za unos i postovanje
+    path('added_item/', IndexView.as_view(template_name='added_item.html'), name="added"),  #ovo sam probao al onda dobijam na toj strani isto sto i na indexu
+    #path('added_item/', TemplateView.as_view(template_name="added_item.html"), name="added" ), #ovde samo get metodu - samo prikaz liste
 ]
--- test_app/views.py
+++ test_app/views.py
@@ -3,7 +3,7 @@ from .models import Lik
 from django.views import View
 from .forms import *
 from django.forms import ModelForm
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 class IndexView(View):
@@ -11,15 +11,13 @@ class IndexView(View):
     initial = {'key': 'value'}   #inicijalne
     template_name="index.html"
     def get(self, request, *args, **kwargs):
-
         created = Lik.objects.all()     #ovde vucem podatke iz baze i prikazujem ih posle kroz created:created
         form = self.form(initial=self.initial)    #ovo je forma koju koristim i prikazujem kroz form:form
         return render(request, self.template_name, {"created":created,"form":form})
 
-    def post(self, request, *args, **kwargs):
-        template_name = 'added_item.html'
+    def post(self, request, num=1, *args, **kwargs):
         form = self.form(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('added')  #ovde mozda gresim
+            return HttpResponseRedirect('added_item/')  #ovde mozda gresim
         return render(request, self.template_name, {"form":form})    #ovde mozda gresim
